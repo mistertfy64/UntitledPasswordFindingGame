43220fed18129ef420c2f5860759179f935e110a
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-05-03 23:00:29 +0700

    fix: fix typo

--- src/server/routes/contest.ts
+++ src/server/routes/contest.ts
@@ -2,8 +2,8 @@ import express from "express";
 import ejs from "ejs";
 const router = express.Router();
 
-router.get("/contest", async (request: express.Request, response) => {
-	response.render("pages/contest", {
+router.get("/contests", async (request: express.Request, response) => {
+	response.render("pages/contests", {
 		authentication: request.authentication,
 		csrfToken: request.generatedCSRFToken,
 		sessionID: request.sessionID,
