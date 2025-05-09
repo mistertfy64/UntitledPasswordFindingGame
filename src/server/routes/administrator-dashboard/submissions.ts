1f7db9fe4bb46c271f89b34fdf2e2c715537f093
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-05-09 12:44:31 +0700

    Reapply "Administrator Dashboard Submission Viewing"
    This reverts commit cbce42f353a1088c462747d25c46e19544bf114d.

--- src/server/routes/administrator-dashboard/submissions.ts
+++ src/server/routes/administrator-dashboard/submissions.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import ejs from "ejs";
 import { log } from "../../utilities/log";
+import { Submission } from "../../models/Submission";
 const router = express.Router();
 
 function authorized(request: express.Request) {
   return request.authentication.ok && request.authentication.isAdministrator;
 }
+
+const LIMIT = 100;
 
 router.get(
   "/administrator/submissions",
@@ -31,17 +34,31 @@ router.get(
 
     const page = Math.max(1, queryPage);
 
-    const submissions = await Submission.getAccordingToQuery(page, amount);
-
-    response.render("pages/submissions.ejs", {
-      authentication: request.authentication,
-      csrfToken: request.generatedCSRFToken,
-      sessionID: request.sessionID,
-      data: submissions,
-      page: page,
-      amount: amount
-    });
-    return;
+    try {
+      const submissions = await Submission.getAccordingToQuery(page, amount);
+      response.render("pages/submissions.ejs", {
+        authentication: request.authentication,
+        csrfToken: request.generatedCSRFToken,
+        sessionID: request.sessionID,
+        data: submissions,
+        page: page,
+        amount: amount
+      });
+      return;
+    } catch (error: unknown) {
+      log.error("Unable to fetch submissions.");
+      if (error instanceof Error) {
+        log.error(error.stack);
+      } else {
+        log.error(error);
+      }
+      response.render("pages/500.ejs", {
+        authentication: request.authentication,
+        csrfToken: request.generatedCSRFToken,
+        sessionID: request.sessionID
+      });
+      return;
+    }
   }
 );
 
