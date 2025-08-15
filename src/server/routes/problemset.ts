7010022c4f75cab9b5ef71d3492163bc24025c4d
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-08-15 22:55:47 +0700

    feature: selectable detail for each problem

--- src/server/routes/problemset.ts
+++ src/server/routes/problemset.ts
@@ -6,12 +6,32 @@ const router = express.Router();
 router.get("/problemset", async (request: express.Request, response) => {
   const problems = await Problem.getVisibleProblems();
   problems.sort((a, b) => a.problemNumber - b.problemNumber);
+  const detail = await getDetailToShow(request);
   response.render("pages/problemset", {
     authentication: request.authentication,
     problems: problems,
     csrfToken: request.generatedCSRFToken,
-    sessionID: request.sessionID
+    sessionID: request.sessionID,
+    detail: detail
   });
 });
+
+async function getDetailToShow(request: express.Request) {
+  const detail = request.query.detail;
+  switch (detail) {
+    case "solved": {
+      return { header: "Solved", value: "solved" };
+    }
+    case "difficulty": {
+      return { header: "Difficulty", value: "difficulty" };
+    }
+    case "category": {
+      return { header: "Category", value: "category" };
+    }
+    default: {
+      return { header: "Solved", value: "solved" };
+    }
+  }
+}
 
 export { router };
