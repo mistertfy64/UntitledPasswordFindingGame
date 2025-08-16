78acde123a97a92c96d06dc2740aafc854286637
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2025-08-16 14:18:18 +0700

    fix: better sanitize index.ts content

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import markdownit from "markdown-it";
 import { Announcement } from "../models/Announcement";
+import DOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 
 const router = express.Router();
-const md = markdownit();
+const md = markdownit({ html: false });
+const window = new JSDOM("").window;
+const purify = DOMPurify(window);
 
 router.get("/", async (request: express.Request, response) => {
   const announcements = await Announcement.getVisibleAnnouncements(5);
   for (const announcement of announcements) {
-    announcement.body = md.render(announcement.body);
+    const rendered = md.render(announcement.body);
+    announcement.body = purify.sanitize(rendered, {
+      USE_PROFILES: { html: true }
+    });
   }
   response.render("pages/index", {
     authentication: request.authentication,
