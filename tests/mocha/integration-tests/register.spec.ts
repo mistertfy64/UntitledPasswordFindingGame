e1905c0ee65b51efe69d33e6a4df67fa536b06fc
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-05-24 19:40:45 +0700

    test: add hooks to tests for `register.spec.ts`

--- tests/mocha/integration-tests/register.spec.ts
+++ tests/mocha/integration-tests/register.spec.ts
@@ -8,4 +8,37 @@ import { createWebServer } from "../../../src/server";
 const request = require("supertest");
 const cheerio = require("cheerio");
 
-describe("/register", ()=>{});
+describe("/register", () => {
+  let databaseConnection: mongoose.Mongoose;
+
+  before(async function () {
+    databaseConnection = await mongoose.connect(process.env.DATABASE_URI ?? "");
+    mongoose.connection.on("connected", () => {
+      console.log(`Connected to test database!`);
+    });
+  });
+
+
+  beforeEach(async function () {
+    // add test user
+    const user = new User();
+    user.username = TESTING_CONSTANTS.TESTING_USER_USERNAME;
+    user.passwordHash = await bcrypt.hash(
+      TESTING_CONSTANTS.TESTING_USER_PASSWORD,
+      4
+    );
+    await user.save();
+  });
+
+
+
+  afterEach(async function () {
+    await databaseConnection.connection.db.dropDatabase();
+  });
+
+  after(async function () {
+    await databaseConnection.connection.close();
+  });
+}
+
+});
